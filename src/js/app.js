d65d277addca6d31adec0814dde211aeb9876414
Gabriel Riveros <velo02.gr@gmail.com>
2024-04-24 13:16:01 -0400

    Agregar seleccion del DOM de los elementos faltantes

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,13 @@
 const btnNumbers = document.getElementsByName('data-number');
 const operator = document.getElementsByName('operator');
+const igual = document.getElementsByName('equal')[0];
+const punto = document.getElementsByName('punto')[0];
+const borrar = document.getElementsByName('erase');
+let result = document.getElementById('result');
+
+console.log(btnNumbers)
+console.log(operator)
+console.log(igual)
+console.log(punto)
+console.log(borrar)
+console.log(result)
