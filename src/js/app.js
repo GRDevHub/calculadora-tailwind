2598c2e354ab280a20936dbea5d2ce676e8e1bc0
Gabriel Riveros <velo02.gr@gmail.com>
2024-04-24 20:22:40 -0400

    Agregar funcionalidad a la calculadora

--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,105 @@
 const btnNumbers = document.getElementsByName('data-number');
 const operator = document.getElementsByName('operator');
-const igual = document.getElementsByName('equal')[0];
+const equal = document.getElementsByName('equal')[0];
 const punto = document.getElementsByName('punto')[0];
 const borrar = document.getElementsByName('erase');
 let result = document.getElementById('result');
+let opeActual = '';
+let opeAnterior = '';
+let operacion = undefined;
 
-console.log(btnNumbers)
-console.log(operator)
-console.log(igual)
-console.log(punto)
-console.log(borrar)
-console.log(result)
+btnNumbers.forEach( btn => {
+    btn.addEventListener('click', function(){
+        addNumero(btn.innerHTML);
+    });
+});
+
+operator.forEach( op => {
+    op.addEventListener('click', function(){
+        addOperator(op.innerHTML);
+    })
+});
+
+equal.addEventListener('click', function(){
+    outputResult();
+    updateDisplay();
+});
+
+borrar.forEach(btn => {
+    btn.addEventListener('click', function(e){
+        clear(e.target);
+    })
+});
+
+punto.addEventListener('click', function(){
+    addNumero(punto.innerHTML);
+    // alert(punto.innerHTML);
+})
+
+const clear = button => {
+    // console.log(button.innerHTML);
+    if(button.innerHTML === 'C'){
+        opeActual = '';
+        opeAnterior = '';
+        operacion = undefined;
+        updateDisplay();
+    }else{
+        opeActual = opeActual.substring(0, opeActual.length - 1);
+        updateDisplay();
+    }
+}
+
+const addOperator = (op) => {
+    if(op === '') return;
+    if(opeAnterior !== ''){
+        outputResult();
+    } 
+    operacion = op.toString();
+    opeAnterior = opeActual;
+    opeActual = '';
+}
+
+const outputResult = () => {
+    let calculo;
+    const anterior = parseFloat(opeAnterior);
+    const actual = parseFloat(opeActual);
+    if(isNaN(anterior) || isNaN(actual)) return;
+    switch(operacion){
+        case '+':
+            calculo = anterior + actual;
+            break;
+        case '-':
+            calculo = anterior - actual;
+            break;
+        case 'x':
+            calculo = anterior * actual;
+            break;
+        case '/':
+            calculo = anterior / actual;
+            break;
+        default:
+            return;
+    }
+    opeActual = calculo.toString();
+    operacion = undefined;
+    opeAnterior = '';
+    // updateDisplay();
+}
+
+const addNumero = (num) => {
+    console.log(opeActual.substring(opeActual.length - 1))
+    if(num === '.'){
+        if(opeActual === '') return;
+        if(opeActual !== '' && opeActual.substring(opeActual.length - 1) === '.') return;
+        opeActual = opeActual.toString() + num.toString();
+        updateDisplay();
+        
+    }else{
+        opeActual = opeActual.toString() + num.toString();
+        updateDisplay();
+    }
+}
+
+const updateDisplay = () => {
+    result.value = opeActual;
+}
